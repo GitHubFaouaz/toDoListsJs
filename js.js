4d0f1124c916d008710f0e5c20e206de9396613c
faouaz <faouaza@yahoo.com>
2023-09-21 20:57:14 +0200

    TOGGLE

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
 let inputBx = document.querySelector("#inputBx");
 let list = document.querySelector("#list");
 
+// L'événement keyup est un événement JavaScript qui se déclenche lorsqu'une touche du clavier est relâchée  Cet événement est souvent utilisé pour détecter les actions de l'utilisateur liées au clavier, telles que la saisie de texte.
 inputBx.addEventListener("keyup", function (event) {
+  console.log("event", event);
+  //  La fonction de rappel commence par vérifier si la touche qui a été relâchée est la touche "Enter" (touche Entrée) en utilisant event.key
   if (event.key == "Enter") {
-    addItem(this.value);
-    this.value = "";
+    addItem(this.value); //Si la touche "Enter" est pressée, la fonction addItem(this.value) est appelée
+    this.value = ""; // Après avoir ajouté l'élément à la liste, la valeur du champ de texte est réinitialisée à une chaîne vide (this.value = "")
   }
 });
 
@@ -16,9 +19,9 @@ let addItem = (inputBx) => {
     this.classList.toggle("done");
   });
 
-  listItem.querySelector("i").addEventListener("click", function () {
-    listItem.remove();
-  });
+  // listItem.querySelector("i").addEventListener("click", function () {
+  //   listItem.remove();
+  // });
 
   list.appendChild(listItem);
 };
